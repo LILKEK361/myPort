f0a032e4c4ea182b7d88b3fef8f31580c1fc89c7
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-05-03 10:44:04 +0200

    starting

--- myportfolio/src/routes/+page.svelte
+++ myportfolio/src/routes/+page.svelte
@@ -1,1 +1,3 @@
-<div style=""> Hello World</div>
+<div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+    <div style="height:90vh; border-color: black; ">Here will be a terminal</div>
+</div>
