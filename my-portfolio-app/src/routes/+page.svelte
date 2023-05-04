bc41cdb679eb4bb1a12bfb105ed9634207ab8af3
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-05-04 18:22:29 +0200

    now displays your command and fixed some tailwindcss

--- my-portfolio-app/src/routes/+page.svelte
+++ my-portfolio-app/src/routes/+page.svelte
@@ -10,11 +10,11 @@
     function onekeydown(e) {
         if (e.code === "Enter") {
             input = terminalInput.value;
-            terminalOutput.innerHTML += `<div class="flex w-100% h-10%">
-											<p class="w-10% ml-[15px] mr-[15px]" >myportfolio@user:</p>
-											<p class="w-90%" ></p>
+            terminalOutput.innerHTML += `<div class="w-[100%] h-[10%] flex">
+											<p class="w-[10%] text-center ml-[15px] mr-[15px]" >myportfolio@user:</p>
+											<p class="w-90% flex" >${terminalInput.value}</p>
 										 </div>`
-            terminalInput.value = "";
+
 
             if (allcomands.includes(input)) {
                 checkInput(input)
@@ -26,6 +26,7 @@
 													</p>
 										 		   </div>`;
             }
+            terminalInput.value = "";
         }
 
     }
@@ -49,7 +50,7 @@
                     break;
                 case "whoami":
                     terminalOutput.innerHTML += `<div class="w-[100%] h-[10%] flex">
-											 		<p class="w-[10%] text-center ml-[15px] mr-[15px]" >myportfolio@user: </p>
+											 		<p class="w-[10%]  ml-[15px] mr-[15px]" >myportfolio@user: </p>
 											 		<p class="flex">
 											 			Hello, my Name is Nick Hillmann.<br>
 											 		 	I am a Webdeveloper who learns<br>
@@ -93,11 +94,12 @@
 
 <div class="container h-full mx-auto flex justify-center items-center bg-surface-900 text-green-700">
     <div class=" w-[90%] h-[90%]  border-2 border-green-400 ">
-        <div class="relative min-h-[95%]  max-h-[95%] bg-surface-900 pt-5 pb-5 overflow-hidden"
+        <div class=" min-h-[95%] max-h-[95%] bg-surface-900 pt-5 pb-5 overflow-scroll"
              bind:this={terminalOutput}></div>
-        <div class="h-[5%] w-[100%] bg-surface-600 flex border-2 border-green-400">
-            <div class="w-[10%] text-center ml-[15px] mr-[15px] h-[50%]">myportfolio@user:</div>
-            <input class="w-[90%] bg-surface-600 pl-2" bind:this={terminalInput} on:keydown={onekeydown}>
+        <div class="h-[5%] w-[100%] bg-surface-600 flex border-2 border-green-400 justify-center items-center ">
+            <div class="w-[10%] h-[100%] ml-[15px] mr-2 mt-3.5 text-center ">myportfolio@user:</div>
+            <input class="w-[90%] h-[100%] bg-surface-600 " bind:this={terminalInput} on:keydown={onekeydown} placeholder="pls enter command here">
         </div>
+
     </div>
 </div>
