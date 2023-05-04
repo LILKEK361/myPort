3f3987de98fcb5d5e907949d199cfeb0dd213a03
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-05-04 10:17:30 +0200

    getting somewhere

---
+++ my-portfolio-app/src/lib/terminalbar.svelte
@@ -0,0 +1,4 @@
+<div class="w-[100%] h-[10%] flex">
+    <p class="w-[10%] text-center ml-[15px] mr-[15px]" >myportfolio@user:</p>
+    <p class="w-[90%] text-left">Hellow</p>
+</div>
--- my-portfolio-app/src/routes/+page.svelte
+++ my-portfolio-app/src/routes/+page.svelte
@@ -1,11 +1,45 @@
+<script lang="ts">
+
+	import terminalbar from "$lib/terminalbar.svelte";
+
+	let input =  "";
+	let terminalInput;
+	let terminalOutput : HTMLAreaElement;
 
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class=" w-[99%] h-[90%]  border-2 border-amber-50">
-		<div class="h-[90%] bg-gray-500"></div>
-		<div class="h-[10%] w-[100%] bg-gray-500 flex">
-			<div class="w-[10%] text-center ml-[15px] mr-[15px] h-[100%]">myportfolio@user:</div>
-			<input class="w-[90%]">
+	const allcomands = ["help","contact", ""]
+
+	function onekeydown(e){
+		if(e.code === "Enter"){
+			input = terminalInput.value;
+			allcomands.includes(input) ? checkInput(input) : "";
+
+			terminalInput.value = "";
+		}
+
+	}
+	function checkInput(input){
+
+		try{
+			terminalOutput.innerHTML += `<div class="w-[100%] h-[10%] flex">
+											 <p class="w-[10%] text-center ml-[15px] mr-[15px]" >myportfolio@user: </p>
+											 <p class="w-[90%] text-left">Hellow</p>
+										 </div>`
+
+		}catch (e) {
+			console.log(e)
+		}
+
+
+	}
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center bg-surface-600">
+	<div class=" w-[99%] h-[90%]  border-2 border-amber-50 ">
+		<div class="relative min-h-[90%]  max-h-[90%] bg-surface-600 pt-5 pb-5" bind:this={terminalOutput} ></div>
+		<div class="h-[10%] w-[100%] bg-surface-600 flex border-tertiary-200-700-token border-amber-50">
+			<div  class="w-[10%] text-center ml-[15px] mr-[15px] h-[100%]">myportfolio@user:</div>
+			<input class="w-[90%]" bind:this={terminalInput} on:keydown={onekeydown}>
 		</div>
 	</div>
 </div>
