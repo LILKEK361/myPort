fba00529c39675e4218cc8659922fb57e002ad83
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-05-04 14:46:05 +0200

    still working in school

--- my-portfolio-app/src/routes/+page.svelte
+++ my-portfolio-app/src/routes/+page.svelte
@@ -1,43 +1,54 @@
 <script lang="ts">
 
-	import help from "$lib/help.svelte";
+    let input: String = "";
+    let terminalInput;
+    let terminalOutput: HTMLAreaElement;
 
 
-	let input : String =  "";
-	let terminalInput;
-	let terminalOutput : HTMLAreaElement;
+    const allcomands = ["help", "contact", "clear", "whoami", "projects",]
 
+    function onekeydown(e) {
+        if (e.code === "Enter") {
+            input = terminalInput.value;
+            terminalOutput.innerHTML += `<div class="flex w-100% h-10%">
+											<p class="w-10% ml-[15px] mr-[15px]" >myportfolio@user:</p>
+											<p class="w-90%" ></p>
+										 </div>`
+            terminalInput.value = "";
 
-	const allcomands = ["help","contact", "clear", "whoami"]
-
-	function onekeydown(e){
-		if(e.code === "Enter"){
-			input = terminalInput.value;
-			allcomands.includes(input) ? checkInput(input) : "";
-
-			terminalInput.value = "";
-		}
-
-	}
-	function checkInput(input) {
-
-		try {
-			switch (input.toLowerCase()) {
-
-				case "help":
-					terminalOutput.innerHTML += `<div class="w-[100%] h-[10%] flex ">
-													<p class="w-[10%] text-center ml-[15px] mr-[15px]" >myportfolio@user: </p>
-													<p class="flex">
-														Help: displays all commands<br>
-														contanct: displays my contacts<br>
-														whoami: displays infos about me<br>
-														clear: clears the terminal<br>
-														projects: displays my GitHub Projects<br>
+            if (allcomands.includes(input)) {
+                checkInput(input)
+            } else {
+                terminalInput.innerHTML += `<div class="w-[100%] h-[10%] flex">
+											 		<p class="w-[10%] text-center ml-[15px] mr-[15px]" >myportfolio@user: </p>
+											 		<p class="">
+											 			please enter a valid command
 													</p>
-												   </div>`
-					break;
-				case "whoami":
-					terminalOutput.innerHTML += `<div class="w-[100%] h-[10%] flex">
+										 		   </div>`;
+            }
+        }
+
+    }
+
+    function checkInput(input) {
+
+        try {
+            switch (input.toLowerCase()) {
+
+                case "help":
+                    terminalOutput.innerHTML += `<div class="w-[100%] h-[10%] flex ">
+                                                    <p class="w-[10%] text-center ml-[15px] mr-[15px]">myportfolio@user: </p>
+                                                    <p class="flex">
+                                                        help: displays all commands<br>
+                                                        contanct: displays my contacts<br>
+                                                        whoami: displays infos about me<br>
+                                                        clear: clears the terminal<br>
+                                                        projects: displays my GitHub Projects<br>
+                                                    </p>
+                                                  </div>`
+                    break;
+                case "whoami":
+                    terminalOutput.innerHTML += `<div class="w-[100%] h-[10%] flex">
 											 		<p class="w-[10%] text-center ml-[15px] mr-[15px]" >myportfolio@user: </p>
 											 		<p class="flex">
 											 			Hello, my Name is Nick Hillmann.<br>
@@ -47,40 +58,46 @@
 											 			open for new ideas and projects<br>
 													</p>
 										 		   </div>`
-					break;
+                    break;
 
-				case "clear":
-					terminalOutput.innerHTML = "";
-					break;
-				case "projects":
-					terminalOutput.innerHTML += `<div class="w-[100%] h-[10%] flex">Hello</div>`
-					break;
+                case "clear":
+                    terminalOutput.innerHTML = "";
+                    break;
+                case "projects":
+                    terminalOutput.innerHTML += `<div class="w-[100%] h-[10%] flex">
+											 		<p class="w-[10%] text-center ml-[15px] mr-[15px]" >myportfolio@user: </p>
+											 		<p class="">
+
+													</p>
+										 		   </div>`
+                    break;
 
 
-				default:
-					terminalOutput.innerHTML += `<div class="w-[100%] h-[10%] flex">
+                default:
+                    terminalOutput.innerHTML += `<div class="w-[100%] h-[10%] flex">
 											 		<p class="w-[10%] text-center ml-[15px] mr-[15px]" >myportfolio@user: </p>
 											 		<p class="">
 											 			please enter a valid command
 													</p>
 										 		   </div>`
 
-					break;
-			}
-		}catch (e) {
-						console.log(e);
-		}
+                    break;
+            }
+        } catch (e) {
+            console.log(e);
+        }
 
 
-	}
+    }
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center bg-surface-900" >
-	<div class=" w-[99%] h-[90%]  border-2 border-green-400 ">
-		<div class="relative min-h-[95%]  max-h-[90%] bg-surface-900 pt-5 pb-5 overflow-hidden" bind:this={terminalOutput}  ></div>
-		<div class="h-[5%] w-[100%] bg-surface-600 flex border-2 border-green-400">
-			<div  class="w-[10%] text-center ml-[15px] mr-[15px] h-[50%]">myportfolio@user:</div>
-			<input class="w-[90%] bg-surface-600" bind:this={terminalInput} on:keydown={onekeydown} >
-		</div>
-	</div>
+<div class="container h-full mx-auto flex justify-center items-center bg-surface-900 text-green-700">
+    <div class=" w-[90%] h-[90%]  border-2 border-green-400 ">
+        <div class="relative min-h-[95%]  max-h-[95%] bg-surface-900 pt-5 pb-5 overflow-hidden"
+             bind:this={terminalOutput}></div>
+        <div class="h-[5%] w-[100%] bg-surface-600 flex border-2 border-green-400">
+            <div class="w-[10%] text-center ml-[15px] mr-[15px] h-[50%]">myportfolio@user:</div>
+            <input class="w-[90%] bg-surface-600 pl-2" bind:this={terminalInput} on:keydown={onekeydown}>
+        </div>
+    </div>
 </div>
