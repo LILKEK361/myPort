aaf38adc003b04e11dcbc02ad6da2c832480cc05
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-05-04 07:55:30 +0200

    changed Css framework

---
+++ my-portfolio-app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class=" w-[99%] h-[90%]  border-2 border-amber-50">
+		<div class="h-[90%] bg-gray-500"></div>
+		<div class="h-[10%] w-[100%] bg-gray-500 flex">
+			<div class="w-[10%] text-center ml-[15px] mr-[15px] h-[100%]">myportfolio@user:</div>
+			<input class="w-[90%]">
+		</div>
+	</div>
+</div>
